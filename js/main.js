7d6b9f6a9911616802de8aef89fd782b6e073469
Heidi Goosen <heidi.goosen@gmail.com>
2016-02-23 06:40:44 +1300

    version 1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    //button click
+    $("button").click(function(){
+        $("p:first").replaceWith("\"Life is uncertain. Eat dessert first.\" -Ernestine Ulmer");
+    });
+
+
+
+});//end of document ready function
