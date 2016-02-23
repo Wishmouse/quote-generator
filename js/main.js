18b4416965b47a6449f054f8f4c97c15dd6a1cc7
Maxine Ellah <maxine.ellah@gmail.com>
2016-02-23 15:15:57 +1300

    add maxine edit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,39 @@ var newQuote = document.getElementById("quote").onclick = Math.random();
 
 
 
+/*switch (newQuote) {
+  case <= 0.1:
+    return "quote1";
+    break;
+  case <= 0.2:
+    return "quote2";
+    break;
+  case <= 0.3:
+    return "quote3";
+    break;
+  case <= 0.4:
+    return "quote4";
+    break;
+  case <= 0.5:
+    return "quote5";
+    break;
+  case <= 0.6:
+    return "quote6";
+    break;
+  case <= 0.7:
+    return "quote7";
+    break;
+  case <= 0.8:
+    return "quote8";
+    break;
+  case <= 0.9:
+    return "quote9";
+    break;
+  case <= 1:
+    return "quote10";
+    break;
 
+}
 
 
 
