d658139b0409c5912165a1324ac256d921d8a414
Maxine Ellah <maxine.ellah@gmail.com>
2016-02-23 17:16:09 +1300

    add random number generator

--- js/main.js
+++ js/main.js
@@ -1,12 +1,57 @@
 //var newQuote = document.getElementById("quote").onclick = Math.random();
 
 function randomNumber() {
-    document.getElementById("quote").onclick = console.log(Math.random());
+    document.getElementById("quote").innerHTML = (Math.random());
 }
 
-var newQuote = randomNumber();
 
 
+ if (randomNumber <= 0.1) {
+    randomNumber == "quote1";
+  } else if (randomNumber <= 0.2) {
+    randomNumber == "quote2"; 
+  } else if (randomNumber <= 0.3) {
+    randomNumber == "quote3";
+  } else if (randomNumber <= 0.4) {
+    randomNumber == "quote4";
+  } else if (randomNumber <= 0.5) {
+    randomNumber == "quote5";
+  } else if (randomNumber <= 0.6) {
+    randomNumber == "quote6";
+  }
+
+ /*switch (newQuote) {
+   case <= 0.1:
+    return "";
+    break;
+  case <= 0.2:
+    return "quote2";
+    break;
+  case <= 0.3:
+    return "quote3";
+    break;
+  case <= 0.4:
+    return "quote4";
+    break;
+  case <= 0.5:
+    return "quote5";
+    break;
+  case <= 0.6:
+    return "quote6";
+    break;
+  case <= 0.7:
+    return "quote7";
+    break;
+  case <= 0.8:
+    return "quote8";
+    break;
+  case <= 0.9:
+    return "quote9";
+    break;
+  case <= 1:
+    return "quote10";
+    break;
+} */
 
 /*switch (newQuote) {
   case <= 0.1:
