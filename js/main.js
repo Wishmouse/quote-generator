16cdc86ae81fe670d4e82a350861325586a478c0
Maxine Ellah <maxine.ellah@gmail.com>
2016-02-23 14:09:06 +1300

    push maxine changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-$(document).ready(function(){
+var newQuote = document.getElementById("quote").onclick = Math.random();
+
+
+
+
+
+
+
+
+
+
+//$(document).ready(function(){
     //button click
-    $("button").click(function(){
-        $("p:first").replaceWith("\"Life is uncertain. Eat dessert first.\" -Ernestine Ulmer");
-    });
+    //$("button").click(function(){
+        //$("p:first").replaceWith("\"Life is uncertain. Eat dessert first.\" -Ernestine Ulmer");
+    //});
 
 
 
-});//end of document ready function
+//});//end of document ready function
